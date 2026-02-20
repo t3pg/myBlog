9b8ebaf69eef3cec0fc941c2e8c1aa05c2c20557
Ryuma <T1_@outlook.jp>
2026-02-21 02:07:29 +0900

    ヒーローイメーの設定に当たって変更する場所を間違えていたので修正。

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://t-log.pages.dev',
 	integrations: [astroExpressiveCode(), mdx(), sitemap(), starlight({
 		title: 't-log'
 	})],

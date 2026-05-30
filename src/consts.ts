b61a2017a6836a8f1aec2557bbeac480805687f2
t3pg <t2411663@gl.cc.uec.ac.jp>
2026-05-30 17:51:45 +0900

    サイトの説明を追加

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 't-log';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = '日記兼技術メモ';
 
 // サイドバーに表示するタグ（手動設定）
 export const SIDEBAR_TAGS = [
